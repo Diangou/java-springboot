a23e0ebdb518e806721d08afc49632858cae860a
dydypink09@gmail.com <diangou.cmr09@gmail.com>
2026-09-18 13:50:25 +0200

    swagger + read me

--- src/main/java/com/bibliothque/biblio/controller/AdherentController.java
+++ src/main/java/com/bibliothque/biblio/controller/AdherentController.java
@@ -2,10 +2,13 @@ package com.bibliothque.biblio.controller;
 
 import com.bibliothque.biblio.model.Adherent;
 import com.bibliothque.biblio.service.AdherentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Adherent", description = "Gestion des adherents et de leur carte")
 @RestController
 @RequestMapping("/api/adherent")
 public class AdherentController {
@@ -16,26 +19,31 @@ public class AdherentController {
         this.adherentService = adherentService;
     }
 
+    @Operation(summary = "Creer un adherent", description = "Cree un adherent, avec une carte optionnelle si un numero de carte est fourni")
     @PostMapping
     public Adherent ajouterAdherent(@RequestBody AdherentRequest request) {
         return adherentService.ajouter(request.nom(), request.prenom(), request.carte());
     }
 
+    @Operation(summary = "Lister les adherents")
     @GetMapping
     public List<Adherent> recupereAdherent() {
         return adherentService.recupereTousLesAdherents();
     }
 
+    @Operation(summary = "Recuperer un adherent par son id")
     @GetMapping("/{id}")
     public Adherent recupereAdherentParId(@PathVariable("id") int id) {
         return adherentService.recupereAdherentParId(id);
     }
 
+    @Operation(summary = "Supprimer un adherent par son id")
     @DeleteMapping("/{id}")
     public Adherent supprimerAdherentParId(@PathVariable("id") int id) {
         return adherentService.supprimerAdherentParId(id);
     }
 
+    @Operation(summary = "Modifier le nom et prenom d'un adherent")
     @PutMapping("/{id}")
     public Adherent modifierAdherentParId(@PathVariable("id") int id, @RequestBody AdherentRequest request) {
         return adherentService.modifierAdherentParId(id, request.nom(), request.prenom());
--- src/main/java/com/bibliothque/biblio/controller/CDController.java
+++ src/main/java/com/bibliothque/biblio/controller/CDController.java
@@ -2,10 +2,13 @@ package com.bibliothque.biblio.controller;
 
 import com.bibliothque.biblio.model.CD;
 import com.bibliothque.biblio.service.CDService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "CD", description = "Gestion des CD")
 @RestController
 @RequestMapping("/api/cd")
 public class CDController {
@@ -16,26 +19,31 @@ public class CDController {
         this.cdService = cdService;
     }
 
+    @Operation(summary = "Creer un CD")
     @PostMapping
     public CD ajouterCD(@RequestBody CDRequest request) {
         return cdService.ajouter(request.titre());
     }
 
+    @Operation(summary = "Lister les CD")
     @GetMapping
     public List<CD> recupereCD() {
         return cdService.recupereTousLesCD();
     }
 
+    @Operation(summary = "Recuperer un CD par son id")
     @GetMapping("/{id}")
     public CD recupereCDParId(@PathVariable("id") int id) {
         return cdService.recupereCDParId(id);
     }
 
+    @Operation(summary = "Supprimer un CD par son id")
     @DeleteMapping("/{id}")
     public CD supprimerCDParId(@PathVariable("id") int id) {
         return cdService.supprimerCDParId(id);
     }
 
+    @Operation(summary = "Modifier le titre d'un CD")
     @PutMapping("/{id}")
     public CD modifierCDParId(@PathVariable("id") int id, @RequestBody CDRequest request) {
         return cdService.modifierCDParId(id, request.titre());
